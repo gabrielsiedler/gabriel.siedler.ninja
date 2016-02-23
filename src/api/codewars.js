61f59d187539d08fc855a65546f262efe40a1216
Gabriel Siedler <gabrielsiedler@gmail.com>
2016-02-23 18:23:05 -0300

    Improve codewars api gathering

--- src/api/codewars.js
+++ src/api/codewars.js
@@ -3,9 +3,21 @@ const rp = require('request-promise');
 const router = module.exports = express.Router();
 
 router.get('/', (req, res) => {
-  rp('https://www.codewars.com/api/v1/users/gabrielsiedler')
+  const request = {
+    uri: 'https://www.codewars.com/api/v1/users/gabrielsiedler',
+    json: true
+  };
+
+  rp(request)
     .then(data => {
-      res.send(data);
+      res.send({
+        honor: data.honor,
+        leaderboardPosition: data.leaderboardPosition,
+        rank: data.ranks.overall.name,
+        score: data.ranks.overall.score,
+        authored: data.codeChallenges.totalAuthored,
+        completed: data.codeChallenges.totalCompleted
+      });
     })
     .catch(err => {
       console.log(`ERROR: ${err}`)
