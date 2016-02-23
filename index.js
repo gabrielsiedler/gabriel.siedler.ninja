3fc11460d96c8ad61f4e78a89273d2cd65a90c97
Gabriel Siedler <gabrielsiedler@gmail.com>
2016-02-23 14:56:35 -0300

    Add VIRTUAL_HOST and VIRTUAL_PORT fallback if there is no ENV set

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var host = process.env.VIRTUAL_HOST;
-var port = process.env.VIRTUAL_PORT;
+var host = process.env.VIRTUAL_HOST || 'localhost';
+var port = process.env.VIRTUAL_PORT || '8090';
 
 app.listen(port);
 
-console.log('Serving static content at',host+':'+port);
+console.log('Serving static content at '+host+':'+port);
