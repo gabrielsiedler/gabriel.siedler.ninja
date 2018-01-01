8a0b5b1ceb7e761aae48832b3003505d04a6dc55
Gabriel Siedler <gabrielsiedler@gmail.com>
2018-01-01 17:41:06 -0200

    Fixing og

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ export default class MyDocument extends Document {
           <meta property="og:type" content="website" />
           <meta
             property="og:image"
-            content="http://http://gabriel.siedler.ninja/static/images/og.png"
+            content="http://gabriel.siedler.ninja/static/images/og.jpg"
           />
           <meta
             property="og:description"
